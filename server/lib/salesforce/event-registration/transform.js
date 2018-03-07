79677e85c64a8af9a5bd4ef264fb89d6634823a0
hanlin <hanlaung@stanford.edu>
2018-03-07 01:02:22 -0800

    Updated transform.js for event reg

--- server/lib/salesforce/event-registration/transform.js
+++ server/lib/salesforce/event-registration/transform.js
@@ -24,6 +24,13 @@ export function transformFromSalesforce (eventRegistration) {
             || attribute === 'Unique_services__c'
             || attribute === 'hasUniqueService__c'
             || attribute === 'wellBeing__c'
+            || attribute === 'Learned_About_Event__c',
+            || attribute === 'Seen_Doctor__c',
+            || attribute === 'General_health__c',
+            || attribute === 'Skin_Health__c',
+            || attribute === 'Dignity_Confidence__c',
+            || attribute === 'Dental_Hygiene__c',
+            || attribute === 'Hygiene__c'
         )
     ) {
       transformed[SALESFORCE_FIELD_TO_FORM_FIELD[attribute]] = eventRegistration[attribute]
